7cb4ec4de6f3897d45982a1e3be5ac4f3261551e
RxkerTV <rykeradk@gmail.com>
2024-07-20 14:42:37 -0700

    Flashlight/NightVision working

--- Assets/Scripts/Movement/LookMode.cs
+++ Assets/Scripts/Movement/LookMode.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-//Gives us acsess to post processing 
+//Gives us access to post processing 
 using UnityEngine.Rendering.PostProcessing;
 
 public class LookMode : MonoBehaviour
@@ -24,90 +24,50 @@ public class LookMode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
         vol = GetComponent<PostProcessVolume>();
         flashlight = GameObject.Find("flashlight").GetComponent<Light>();
         flashlight.enabled = false;
         nightVisionOverlay.SetActive(false);
         vol.profile = standard;
         cam = GameObject.Find("PlayerCam").GetComponent<Camera>();
-
     }
-
-
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(N))
         {
-            // nightVisionOn = !nightVisionOn;  // Toggle the state
-            if (nightVisionOn == false)
-            {
-                vol.profile = nightVision;
-                nightVisionOverlay.SetActive(true);
-                nightVisionOn = true;
-                NightVisionOff();
-                Debug.Log("Onnow");
-            }
-            else if (nightVisionOn == true)
-            {
-                vol.profile = standard;
-                nightVisionOverlay.SetActive(false);
-                cam.fieldOfView = 60;
-                nightVisionOn = false;
-                Debug.Log("Offnow");
-
-            }
+            nightVisionOn = !nightVisionOn;
+            ToggleNightVision(nightVisionOn);
         }
 
         if (Input.GetKeyDown(F))
         {
-
-            if (flashLightOn == false)
-            {
-                flashlight.enabled = true;
-                flashLightOn = true;
-                FlashLightSwitchOff();
-                Debug.Log("FOnnow");
-            }
-
-            else if (flashLightOn == true)
-            {
-                flashlight.enabled = false;
-                flashLightOn = false;
-                Debug.Log("FOffnow");
-            }
-
-        }
-    
-        if (nightVisionOn == true)
-        {
-            NightVisionOff();
-        }
-
-        if (flashLightOn == true)
-        {
-            FlashLightSwitchOff();
+            flashLightOn = !flashLightOn;
+            ToggleFlashlight(flashLightOn);
         }
     }
-        private void NightVisionOff()
+
+    private void ToggleNightVision(bool state)
+    {
+        if (state)
         {
-            {
+            vol.profile = nightVision;
+            nightVisionOverlay.SetActive(true);
+            Debug.Log("Onnow");
+        }
+        else
+        {
             vol.profile = standard;
             nightVisionOverlay.SetActive(false);
             cam.fieldOfView = 60;
-            nightVisionOn = false;
+            Debug.Log("Offnow");
         }
-        }
-        private void FlashLightSwitchOff()
-        {
-         flashlight.enabled = false;
-            flashLightOn = false;
-        }
+    }
 
-        }
-
-
-
-
+    private void ToggleFlashlight(bool state)
+    {
+        flashlight.enabled = state;
+        Debug.Log(state ? "FOnnow" : "FOffnow");
+    }
+}
